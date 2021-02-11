bdd1ab384ab4d5a689868917913fdf18a9503e27
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2021-02-11 15:42:26 +0100

    Opprettet sak-service som foreløpig kun henter inn data fra SAF.
    PB-605. minesaker-api - Minimal integrasjon mot SAF
    PB-606. minesaker-api - Sette opp grapQL-klient mot SAF

---
+++ src/main/kotlin/no/nav/personbruker/minesaker/api/common/sak/SakService.kt
@@ -0,0 +1,17 @@
+package no.nav.personbruker.minesaker.api.common.sak
+
+import no.nav.personbruker.minesaker.api.common.AuthenticatedUser
+import no.nav.personbruker.minesaker.api.saf.SafConsumer
+import no.nav.personbruker.minesaker.api.saf.dto.out.Sakstema
+import no.nav.personbruker.minesaker.api.saf.queries.HentKonkretSakstema
+
+class SakService(
+    private val safConsumer: SafConsumer
+) {
+
+    suspend fun hentAltForSakstema(sakstemakode: String): List<Sakstema> {
+        val sakstemaRequest = HentKonkretSakstema.createRequest(sakstemakode)
+        return safConsumer.hentKonkretSakstema(sakstemaRequest)
+    }
+
+}
---
+++ src/test/kotlin/no/nav/personbruker/minesaker/api/sak/SakServiceTest.kt
@@ -0,0 +1,58 @@
+package no.nav.personbruker.minesaker.api.sak
+
+import io.mockk.*
+import kotlinx.coroutines.runBlocking
+import no.nav.personbruker.minesaker.api.common.exception.SafException
+import no.nav.personbruker.minesaker.api.common.sak.SakService
+import no.nav.personbruker.minesaker.api.saf.SafConsumer
+import no.nav.personbruker.minesaker.api.saf.queries.HentKonkretSakstema
+import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.`should be instance of`
+import org.amshove.kluent.`should contain`
+import org.junit.jupiter.api.Test
+
+internal class SakServiceTest {
+
+    @Test
+    fun `Skal hente alle data for et konkret sakstema`() {
+        val expectedSakstemakode = "FOR"
+
+        val consumer = mockk<SafConsumer>(relaxed = true)
+        val service = SakService(consumer)
+
+        val parameterSendtVidere = slot<HentKonkretSakstema>()
+
+        runBlocking {
+            service.hentAltForSakstema(expectedSakstemakode)
+        }
+
+        coVerify(exactly = 1) { consumer.hentKonkretSakstema(capture(parameterSendtVidere)) }
+
+        parameterSendtVidere.captured `should be instance of` HentKonkretSakstema::class
+        parameterSendtVidere.captured.variables.entries.toString() `should contain` expectedSakstemakode
+
+        confirmVerified(consumer)
+    }
+
+    @Test
+    fun `Feil som oppstaar ved henting av sakstema skal kastes videre`() {
+        val expectedException = SafException("Simulert feil i en test")
+
+        val consumer = mockk<SafConsumer>(relaxed = true)
+        val service = SakService(consumer)
+
+        coEvery {
+            consumer.hentKonkretSakstema(any())
+        } throws expectedException
+
+        val result = runCatching {
+            runBlocking {
+                service.hentAltForSakstema("dummykode")
+            }
+        }
+
+        result.isFailure `should be equal to` true
+        result.exceptionOrNull() `should be instance of` SafException::class
+    }
+
+}
