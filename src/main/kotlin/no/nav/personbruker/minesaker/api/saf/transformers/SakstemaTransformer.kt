a491aa00f5fa2b19d3d75f6fd64cb54487554f65
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2021-02-25 13:51:04 +0100

    Innførte en ResultTransformer som har ansvar for å route hver resultattype til korrekt sett av transformere.
    PB-606. minesaker-api - Sette opp graphQL-klient mot SAF

--- src/main/kotlin/no/nav/personbruker/minesaker/api/saf/transformers/SakstemaTransformer.kt
+++ src/main/kotlin/no/nav/personbruker/minesaker/api/saf/transformers/SakstemaTransformer.kt
@@ -4,7 +4,7 @@ import no.nav.dokument.saf.selvbetjening.generated.dto.HentJournalposter
 import no.nav.personbruker.minesaker.api.common.exception.MissingFieldException
 import no.nav.personbruker.minesaker.api.saf.domain.Sakstema
 
-object HentJournalposterTransformer {
+object SakstemaTransformer {
 
     fun toInternal(externalTemaer: List<HentJournalposter.Sakstema>): List<Sakstema> {
         return externalTemaer.map { external ->
@@ -15,14 +15,9 @@ object HentJournalposterTransformer {
     fun toInternal(external: HentJournalposter.Sakstema): Sakstema {
         return Sakstema(
             external.navn ?: throw MissingFieldException("navn"),
-            external.kode ?: throw MissingFieldException("kode")
+            external.kode ?: throw MissingFieldException("kode"),
+            JournalpostTransformer.toInternal(external.journalposter)
         )
-    }
-
-    fun toInternal(externalData: HentJournalposter.Result): List<Sakstema> {
-        return externalData.dokumentoversiktSelvbetjening.tema.map { external ->
-            toInternal(external)
-        }
     }
 
 }
--- src/test/kotlin/no/nav/personbruker/minesaker/api/saf/transformers/SakstemaTransformerTest.kt
+++ src/test/kotlin/no/nav/personbruker/minesaker/api/saf/transformers/SakstemaTransformerTest.kt
@@ -2,20 +2,25 @@ package no.nav.personbruker.minesaker.api.saf.transformers
 
 
 import no.nav.dokument.saf.selvbetjening.generated.dto.HentJournalposter
+import no.nav.personbruker.minesaker.api.saf.domain.JournalpostObjectMother
 import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.`should not be empty`
 import org.amshove.kluent.shouldNotBeNull
 import org.junit.jupiter.api.Test
 
-internal class HentJournalposterTransformerTest {
+internal class SakstemaTransformerTest {
 
     @Test
     fun `Skal kunne transformere fra ekstern til intern modell - Hent konkret sakstema`() {
-        val external = HentJournalposter.Sakstema("navn", "kode", emptyList())
+        val journalposter = listOf(JournalpostObjectMother.giveMeOneInngaaendeDokument())
+        val external = HentJournalposter.Sakstema("navn", "kode", journalposter)
 
-        val internal = HentJournalposterTransformer.toInternal(external)
+        val internal = SakstemaTransformer.toInternal(external)
 
         internal.navn `should be equal to` external.navn
         internal.kode `should be equal to` external.kode
+        internal.journalposter.`should not be empty`()
+        internal.journalposter.size `should be equal to` 1
     }
 
     @Test
@@ -25,7 +30,7 @@ internal class HentJournalposterTransformerTest {
             HentJournalposter.Sakstema("navn2", "kode2", emptyList())
         )
 
-        val internals = HentJournalposterTransformer.toInternal(externals)
+        val internals = SakstemaTransformer.toInternal(externals)
 
         internals.size `should be equal to` externals.size
         internals.forEach { internal ->
