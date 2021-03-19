b703c1dc3efeddf8d29742955c66132a7d6247fd
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2021-03-19 16:29:58 +0100

    Legger til korrekt logging av `GraphQLResultException`s.
    PB-652. minesaker-api - Feilhåndtering hvis ting feiler i SAF

--- src/main/kotlin/no/nav/personbruker/minesaker/api/common/ExceptionResponseHandler.kt
+++ src/main/kotlin/no/nav/personbruker/minesaker/api/common/ExceptionResponseHandler.kt
@@ -3,6 +3,7 @@ package no.nav.personbruker.minesaker.api.common
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
+import no.nav.personbruker.minesaker.api.common.exception.GraphQLResultException
 import no.nav.personbruker.minesaker.api.common.exception.InvalidRequestException
 import no.nav.personbruker.minesaker.api.common.exception.SafException
 import org.slf4j.Logger
@@ -12,6 +13,11 @@ suspend fun ApplicationCall.respondWithError(log: Logger, exception: Exception) 
         is InvalidRequestException -> {
             respond(HttpStatusCode.BadRequest)
             val msg = "Mottok en request med feil input. context={}"
+            log.warn(msg, exception.context, exception)
+        }
+        is GraphQLResultException -> {
+            respond(HttpStatusCode.ServiceUnavailable)
+            val msg = "Klarte ikke å hente data fra SAF. Returnerer feilkode til frontend. context={}"
             log.warn(msg, exception.context, exception)
         }
         is SafException -> {
