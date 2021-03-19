0ae46a85a355e115a781519c12dbdb090ed0107e
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2021-03-19 16:17:28 +0100

    Fjerner SAF sitt felt for feilkoder, fordi de nå får bort fra å bruke dette feltet. Vi kan nå GraphQL sin standard måte for å rapportere feil, nemlig gjennom error- og extensions-listene på resultat et for spørringen.
    PB-652. minesaker-api - Feilhåndtering hvis ting feiler i SAF

--- src/test/kotlin/no/nav/personbruker/minesaker/api/saf/ResultObjectMother.kt
+++ src/test/kotlin/no/nav/personbruker/minesaker/api/saf/ResultObjectMother.kt
@@ -6,31 +6,28 @@ import no.nav.personbruker.minesaker.api.saf.journalposter.objectmothers.Sakstem
 
 object ResultObjectMother {
 
-    private const val defaultResultStatusCode = "ok"
-
     fun giveMeHentSakstemaResult(): HentSakstemaer.Result {
         val temaer = no.nav.personbruker.minesaker.api.saf.sakstemaer.SakstemaObjectMother.giveMeListOfSakstema()
-        val dokumentoversikt = HentSakstemaer.Dokumentoversikt(defaultResultStatusCode, temaer)
+        val dokumentoversikt = HentSakstemaer.Dokumentoversikt(temaer)
         return HentSakstemaer.Result(dokumentoversikt)
     }
 
     fun giveMeHentSakstemaResultMedUfullstendigeData(): HentSakstemaer.Result {
         val sakstemaUtenKodeSatt =
             no.nav.personbruker.minesaker.api.saf.sakstemaer.SakstemaObjectMother.giveMeOneSakstema(kode = null)
-        val dokumentoversikt = HentSakstemaer.Dokumentoversikt(defaultResultStatusCode, listOf(sakstemaUtenKodeSatt))
+        val dokumentoversikt = HentSakstemaer.Dokumentoversikt(listOf(sakstemaUtenKodeSatt))
         return HentSakstemaer.Result(dokumentoversikt)
     }
 
     fun giveMeHentJournalposterResult(): HentJournalposter.Result {
         val temaer = SakstemaObjectMother.giveMeListOfSakstemaer()
-        val dokumentoversikt = HentJournalposter.Dokumentoversikt(defaultResultStatusCode, temaer)
+        val dokumentoversikt = HentJournalposter.Dokumentoversikt(temaer)
         return HentJournalposter.Result(dokumentoversikt)
     }
 
     fun giveMeHentJournalposterResultMedUfullstendigeData(): HentJournalposter.Result {
         val sakstemaUtenKodeSatt = SakstemaObjectMother.giveMeSakstemaWithUtgaaendeDokument(kode = null)
-        val responseCodeIndicatingError = "not_found"
-        val dokumentoversikt = HentJournalposter.Dokumentoversikt(responseCodeIndicatingError, listOf(sakstemaUtenKodeSatt))
+        val dokumentoversikt = HentJournalposter.Dokumentoversikt(listOf(sakstemaUtenKodeSatt))
         return HentJournalposter.Result(dokumentoversikt)
     }
 
