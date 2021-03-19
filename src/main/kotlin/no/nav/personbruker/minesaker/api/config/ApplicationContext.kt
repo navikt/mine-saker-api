aa9031535436b4b42d1ae62509fe9c72310c9572
Christopher Santana <christopher.santana@nav.no>
2021-03-19 14:22:52 +0100

    Bump versjon av tms-ktor-token-support.

--- src/main/kotlin/no/nav/personbruker/minesaker/api/config/ApplicationContext.kt
+++ src/main/kotlin/no/nav/personbruker/minesaker/api/config/ApplicationContext.kt
@@ -4,7 +4,7 @@ import no.nav.personbruker.minesaker.api.common.sak.SakService
 import no.nav.personbruker.minesaker.api.health.HealthService
 import no.nav.personbruker.minesaker.api.saf.SafConsumer
 import no.nav.personbruker.minesaker.api.tokenx.TokendingsServiceWrapper
-import no.nav.tms.token.support.tokendings.exchange.TokenExchangeServices
+import no.nav.tms.token.support.tokendings.exchange.TokendingsServiceBuilder
 
 class ApplicationContext {
 
@@ -13,7 +13,7 @@ class ApplicationContext {
     val httpClient = HttpClientBuilder.build()
     val healthService = HealthService(this)
 
-    val tokendingsService = TokenExchangeServices.tokendingsService
+    val tokendingsService = TokendingsServiceBuilder.buildTokendingsService()
 
     val tokendingsServiceWrapper = TokendingsServiceWrapper(tokendingsService, environment.safClientId)
 
