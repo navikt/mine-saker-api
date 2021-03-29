1936365e39ffeaf9f7a55936839189098021f896
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2021-03-29 14:34:29 +0200

    Robustifisert uthentigen av parameterene som trengs for å hente dokumenter. Har i tillegg lagt til tester for feilhåndteringen som logger og avgjør hvilken feilkode som skal sendes til frontenden.
    PB-651. minesaker-api - hente konkret dokument fra SAF

--- src/main/kotlin/no/nav/personbruker/minesaker/api/common/ExceptionResponseHandler.kt
+++ src/main/kotlin/no/nav/personbruker/minesaker/api/common/ExceptionResponseHandler.kt
@@ -1,33 +1,45 @@
 package no.nav.personbruker.minesaker.api.common
 
-import io.ktor.application.*
 import io.ktor.http.*
-import io.ktor.response.*
 import no.nav.personbruker.minesaker.api.common.exception.GraphQLResultException
 import no.nav.personbruker.minesaker.api.common.exception.InvalidRequestException
 import no.nav.personbruker.minesaker.api.common.exception.SafException
+import no.nav.personbruker.minesaker.api.common.exception.UgyldigVerdiException
 import org.slf4j.Logger
 
-suspend fun ApplicationCall.respondWithError(log: Logger, exception: Exception) {
-    when (exception) {
-        is InvalidRequestException -> {
-            respond(HttpStatusCode.BadRequest)
-            val msg = "Mottok en request med feil input. $exception"
-            log.warn(msg, exception)
-        }
-        is GraphQLResultException -> {
-            respond(HttpStatusCode.ServiceUnavailable)
-            val msg = "Klarte ikke å hente data fra SAF. Returnerer feilkode til frontend. $exception"
-            log.warn(msg, exception)
-        }
-        is SafException -> {
-            respond(HttpStatusCode.ServiceUnavailable)
-            val msg = "Klarte ikke å hente data fra SAF. Returnerer feilkode til frontend. $exception"
-            log.warn(msg, exception)
-        }
-        else -> {
-            respond(HttpStatusCode.InternalServerError)
-            log.error("Ukjent feil oppstod ved henting av eventer. Returnerer feilkode til frontend", exception)
+object ExceptionResponseHandler {
+    fun logExceptionAndDecideErrorResponseCode(log: Logger, exception: Exception): HttpStatusCode {
+        return when (exception) {
+            is InvalidRequestException -> {
+                val errorCode = HttpStatusCode.BadRequest
+                val msg = "Mottok en request med feil input. Returnerer feilkoden $errorCode. $exception"
+                log.warn(msg, exception)
+                errorCode
+            }
+            is GraphQLResultException -> {
+                val errorCode = HttpStatusCode.ServiceUnavailable
+                val msg = "Det skjedde en graphQL-feil. Returnerer feilkoden $errorCode. $exception"
+                log.warn(msg, exception)
+                errorCode
+            }
+            is SafException -> {
+                val errorCode = HttpStatusCode.ServiceUnavailable
+                val msg = "Klarte ikke å hente data. Returnerer feilkoden $errorCode. $exception"
+                log.warn(msg, exception)
+                errorCode
+            }
+            is UgyldigVerdiException -> {
+                val errorCode = HttpStatusCode.InternalServerError
+                val msg = "Det skjedde en feil ved konvertering til den interne-modellen. Returnerer feilkoden $errorCode. $exception"
+                log.warn(msg, exception)
+                errorCode
+            }
+            else -> {
+                val errorCode = HttpStatusCode.InternalServerError
+                val msg = "Ukjent feil oppstod. Returnerer feilkoden $errorCode. $exception"
+                log.error(msg, exception)
+                errorCode
+            }
         }
     }
 }
--- src/main/kotlin/no/nav/personbruker/minesaker/api/common/sak/sakApi.kt
+++ src/main/kotlin/no/nav/personbruker/minesaker/api/common/sak/sakApi.kt
@@ -4,15 +4,17 @@ import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import io.ktor.util.pipeline.*
+import no.nav.personbruker.minesaker.api.common.ExceptionResponseHandler
 import no.nav.personbruker.minesaker.api.common.exception.InvalidRequestException
-import no.nav.personbruker.minesaker.api.common.respondWithError
 import no.nav.personbruker.minesaker.api.config.idportenUser
 import no.nav.personbruker.minesaker.api.saf.domain.DokumentInfoId
 import no.nav.personbruker.minesaker.api.saf.domain.JournalpostId
 import no.nav.personbruker.minesaker.api.saf.domain.Sakstemakode
 import no.nav.personbruker.minesaker.api.saf.domain.toInternalSaktemakode
 import org.slf4j.LoggerFactory
+
+val dokumentIdParameterName = "dokumentId"
+val journalpostIdParameterName = "journalpostId"
 
 fun Route.sakApi(
     service: SakService
@@ -22,12 +24,13 @@ fun Route.sakApi(
 
     get("/journalposter") {
         try {
-            val sakstema = extractOnsketSakstema()
+            val sakstema = call.extractOnsketSakstema()
             val result = service.hentJournalposterForSakstema(idportenUser, sakstema)
             call.respond(HttpStatusCode.OK, result)
 
         } catch (exception: Exception) {
-            call.respondWithError(log, exception)
+            val errorCode = ExceptionResponseHandler.logExceptionAndDecideErrorResponseCode(log, exception)
+            call.respond(errorCode)
         }
     }
 
@@ -37,27 +40,29 @@ fun Route.sakApi(
             call.respond(HttpStatusCode.OK, result)
 
         } catch (exception: Exception) {
-            call.respondWithError(log, exception)
+            val errorCode = ExceptionResponseHandler.logExceptionAndDecideErrorResponseCode(log, exception)
+            call.respond(errorCode)
         }
     }
 
-    get("/dokument/{journalpostId}/{dokumentId}") {
+    get("/dokument/{$journalpostIdParameterName}/{$dokumentIdParameterName}") {
         try {
-            val journapostId = JournalpostId("${call.parameters["journalpostId"]}")
-            val dokumentinfoId = DokumentInfoId("${call.parameters["dokumentId"]}")
-            log.info("Skal hente dokumentet $dokumentinfoId, fra journalposten $journapostId")
-            val result = service.hentDokument(idportenUser, journapostId, dokumentinfoId)
+            val journalpostId = call.extractJournalpostId()
+            val dokumentId = call.extractDokumentInfoId()
+            log.info("Skal hente dokumentet $dokumentId, fra journalposten $journalpostId")
+            val result = service.hentDokument(idportenUser, journalpostId, dokumentId)
             call.respondBytes(bytes = result, contentType = ContentType.Application.Pdf, status = HttpStatusCode.OK)
 
         } catch (exception: Exception) {
-            call.respondWithError(log, exception)
+            val errorCode = ExceptionResponseHandler.logExceptionAndDecideErrorResponseCode(log, exception)
+            call.respond(errorCode)
         }
     }
 
 }
 
-private fun PipelineContext<Unit, ApplicationCall>.extractOnsketSakstema(): Sakstemakode {
-    val sakstemakode: String = call.request.queryParameters["sakstemakode"]
+private fun ApplicationCall.extractOnsketSakstema(): Sakstemakode {
+    val sakstemakode: String = request.queryParameters["sakstemakode"]
         ?: throw InvalidRequestException("Kallet kan ikke utføres uten at tema er valgt.")
 
     val sakstema = runCatching {
@@ -68,4 +73,16 @@ private fun PipelineContext<Unit, ApplicationCall>.extractOnsketSakstema(): Saks
     }
 
     return sakstema.getOrThrow()
+}
+
+private fun ApplicationCall.extractJournalpostId(): JournalpostId {
+    val value = parameters[journalpostIdParameterName]
+        ?: throw InvalidRequestException("Kallet kan ikke utføres uten at '$journalpostIdParameterName' er spesifisert.")
+    return JournalpostId(value)
+}
+
+private fun ApplicationCall.extractDokumentInfoId(): DokumentInfoId {
+    val value = parameters[dokumentIdParameterName]
+        ?: throw InvalidRequestException("Kallet kan ikke utføres uten at '$dokumentIdParameterName' er spesifisert.")
+    return DokumentInfoId(value)
 }
