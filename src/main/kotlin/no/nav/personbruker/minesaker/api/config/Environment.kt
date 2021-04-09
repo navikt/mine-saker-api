d76b03f238853a36fcab66d5dc736ad077122b02
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-04-09 14:07:26 +0200

    La til cors allowed schemes

--- src/main/kotlin/no/nav/personbruker/minesaker/api/config/Environment.kt
+++ src/main/kotlin/no/nav/personbruker/minesaker/api/config/Environment.kt
@@ -4,12 +4,14 @@ import java.net.URL
 
 data class Environment(
     val corsAllowedOrigins: String = getEnvVar("CORS_ALLOWED_ORIGINS"),
+    val corsAllowedSchemes: String = getEnvVar("CORS_ALLOWED_SCHEMES", "https"),
     val safEndpoint: URL = URL(getEnvVar("SAF_API_URL")),
     val safClientId: String = getEnvVar("SAF_CLIENT_ID"),
     val clusterName: String = getEnvVar("NAIS_CLUSTER_NAME")
 )
 
-fun getEnvVar(varName: String): String {
+fun getEnvVar(varName: String, default: String? = null): String {
     return System.getenv(varName)
-        ?: throw IllegalArgumentException("Appen kan ikke starte uten av miljøvariabelen $varName er satt.")
+            ?: default
+            ?: throw IllegalArgumentException("Appen kan ikke starte uten av miljøvariabelen $varName er satt.")
 }
