884c045201fbe9ab7480ae6ffa9a38ae8b1e5162
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2021-04-15 12:34:41 +0200

    Fjernet kompilator-advarsel.
    PB-686. Returner kun nyeste dato

--- src/main/kotlin/no/nav/personbruker/minesaker/api/saf/journalposter/transformers/DateTimeTransformer.kt
+++ src/main/kotlin/no/nav/personbruker/minesaker/api/saf/journalposter/transformers/DateTimeTransformer.kt
@@ -7,4 +7,4 @@ object DateTimeTransformer {
     val timeZoneUTC = "+00:00"
 }
 
-fun DateTime.toInternal() = ZonedDateTime.parse("$this${DateTimeTransformer.timeZoneUTC}")
+fun DateTime.toInternal(): ZonedDateTime = ZonedDateTime.parse("$this${DateTimeTransformer.timeZoneUTC}")
