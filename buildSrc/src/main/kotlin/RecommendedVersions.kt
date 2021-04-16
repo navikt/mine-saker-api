d3805b180cf270d8003fbdadb9e8f0aca10ce924
Christopher Santana <christopher.santana@nav.no>
2021-04-16 15:06:47 +0200

    Bump token-support.

--- buildSrc/src/main/kotlin/RecommendedVersions.kt
+++ buildSrc/src/main/kotlin/RecommendedVersions.kt
@@ -145,7 +145,7 @@ object TestContainers {
 
 object Tms {
     object KtorTokenSupport {
-        private const val version = "2021.04.14-08.19-d6139a0cb38a"
+        private const val version = "2021.04.16-14.34-a9ffd94109fb"
         private const val groupId = "com.github.navikt.tms-ktor-token-support"
 
         const val idporten = "$groupId:token-support-idporten:$version"
