ce0aaf728af3aee3395f6da136b371a3f29bcef9
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2021-05-10 13:53:03 +0200

    Angir hvilket dokument i dokument-listen som er hoveddokumentet. I SAF er det alltid det første dokumentet som returneres som er hoveddokumentet, resten av dokumentene i listen er vedlegg.
    SAF har begynt å levere kun en dokument variant per dokument, slik at vi ikke trenger å ha logikk for hvilken variant som skal vises til sluttbruker.
    
    PB-719. Justeringer: vise journalpost samlet og dokumentene innunder

--- src/main/kotlin/no/nav/personbruker/minesaker/api/saf/domain/Dokumentinfo.kt
+++ src/main/kotlin/no/nav/personbruker/minesaker/api/saf/domain/Dokumentinfo.kt
@@ -3,6 +3,7 @@ package no.nav.personbruker.minesaker.api.saf.domain
 data class Dokumentinfo(
     val tittel: Tittel,
     val dokumentInfoId: DokumentInfoId,
+    val dokumenttype : Dokumenttype,
     val brukerHarTilgang: Boolean,
     val eventuelleGrunnerTilManglendeTilgang : List<String>
 )
