6cdf37fdc3c5b8f1578714be4df2dc729a083fbf
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2021-10-11 14:25:03 +0200

    Skal sjekke at innholdet er likt, ikke at det er samme instans.
    PB-854. Dittnav-api: sett opp unleash for mine saker

--- src/main/kotlin/no/nav/personbruker/minesaker/api/config/NaisEnvironment.kt
+++ src/main/kotlin/no/nav/personbruker/minesaker/api/config/NaisEnvironment.kt
@@ -1,17 +1,12 @@
 package no.nav.personbruker.minesaker.api.config
 
-import org.slf4j.LoggerFactory
-
 object NaisEnvironment {
-
-    private val log = LoggerFactory.getLogger(NaisEnvironment::class.java)
 
     private fun currentClusterName(): String? = System.getenv("NAIS_CLUSTER_NAME")
 
     fun isRunningInProd(): Boolean {
         val clusterName = currentClusterName()
-        log.info("Current cluster: $clusterName")
-        return clusterName === "prod-gcp"
+        return clusterName == "prod-gcp"
     }
 
     fun isRunningInDev(): Boolean {
