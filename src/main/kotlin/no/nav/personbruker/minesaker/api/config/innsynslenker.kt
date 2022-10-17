88570dc36724cbf9b7382e1f951af60ab55a0cb1
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2022-10-17 12:55:57 +0200

    Redirecter sakstema AAP til egen innsynsløsning, i første omgang kun i dev.

--- src/main/kotlin/no/nav/personbruker/minesaker/api/config/innsynslenker.kt
+++ src/main/kotlin/no/nav/personbruker/minesaker/api/config/innsynslenker.kt
@@ -8,6 +8,7 @@ val innsynslenkerDev : Map<Sakstemakode, URL> = mapOf(
     Sakstemakode.DAG to URL("https://arbeid.dev.nav.no/arbeid/dagpenger/mine-dagpenger"),
     Sakstemakode.HJE to URL("https://hjelpemidler.dev.nav.no/hjelpemidler/dinehjelpemidler"),
     Sakstemakode.KOM to URL("https://www-q0.dev.nav.no/sosialhjelp/innsyn"),
+    Sakstemakode.AAP to URL("https://aap-innsyn.dev.nav.no/aap/mine-aap/"),
 )
 
 val generellInnsynslenkeProd = URL("https://person.nav.no/mine-saker/tema/")
