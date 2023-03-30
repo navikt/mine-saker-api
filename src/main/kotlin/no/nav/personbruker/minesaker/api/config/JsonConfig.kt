98724a6b9fce445893b8cc19591fe14cc63f8da4
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2023-03-30 09:10:25 +0200

    mer codecleanup

--- src/main/kotlin/no/nav/personbruker/minesaker/api/config/JsonConfig.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.personbruker.minesaker.api.config
-
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-
-
