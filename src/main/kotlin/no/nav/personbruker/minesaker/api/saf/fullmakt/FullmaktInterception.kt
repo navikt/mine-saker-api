d6fe7f83f47d93932344a08b51cce9eeef02dd92
Christopher Santana <christopher.santana@nav.no>
2023-07-11 13:46:59 +0200

    Sorting.

--- src/main/kotlin/no/nav/personbruker/minesaker/api/saf/fullmakt/FullmaktInterception.kt
+++ src/main/kotlin/no/nav/personbruker/minesaker/api/saf/fullmakt/FullmaktInterception.kt
@@ -1,8 +1,12 @@
 package no.nav.personbruker.minesaker.api.saf.fullmakt
 
+import com.nimbusds.jwt.JWT
+import com.nimbusds.jwt.SignedJWT
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.application.hooks.*
 import io.ktor.server.auth.*
+import io.ktor.server.request.*
 import io.ktor.util.*
 import mu.KotlinLogging
 import no.nav.tms.token.support.idporten.sidecar.authentication.IdPortenTokenPrincipal
@@ -18,11 +22,14 @@ class FullmaktInterception(val fullmektigJwtService: FullmektigJwtService) {
     val interceptor = createApplicationPlugin(name = FullmaktInterceptor) {
         requireNotNull(application.pluginOrNull(Authentication)) { "Fullmaktinterceptor must be installed after Authentication" }
 
-        on(CallSetup) { call ->
+        onCall { call ->
             val fullmektigToken = call.request.cookies[FullmaktCookie]
-            if (fullmektigToken != null) {
+            val accessToken = call.request.authHeader()
+            if (fullmektigToken != null && accessToken != null) {
                 try {
-                    val ident = call.userIdent
+                    val ident = accessToken.userIdent
+
+                    log.info("Token: $fullmektigToken")
 
                     val jwt = fullmektigJwtService.verify(fullmektigToken, ident)
                     val representert = jwt.getClaim(FullmektigJwtService.RepresentertClaim).asString()
@@ -42,7 +49,16 @@ class FullmaktInterception(val fullmektigJwtService: FullmektigJwtService) {
     }
 }
 
-private val ApplicationCall.userIdent get() = principal<IdPortenTokenPrincipal>()?.accessToken?.getClaim("pid")?.asString()!!
+private val JWT.userIdent get() = jwtClaimsSet.getClaim("pid")!! as String
+
+private val authRegex = "Bearer (.+)".toRegex()
+
+private fun ApplicationRequest.authHeader(): JWT? {
+    return headers[HttpHeaders.Authorization]
+        ?.let{ authRegex.matchEntire(it)?.destructured }
+        ?.let { (token) -> token }
+        ?.let { SignedJWT.parse(it) }
+}
 
 data class Fullmakt(
     val fullmektig: String,
--- src/main/kotlin/no/nav/personbruker/minesaker/api/saf/fullmakt/fullmakt.kt
+++ src/main/kotlin/no/nav/personbruker/minesaker/api/saf/fullmakt/fullmakt.kt
@@ -1,14 +1,17 @@
 package no.nav.personbruker.minesaker.api.saf.fullmakt
 
 import java.time.LocalDate
+import java.time.ZonedDateTime
 
 data class FullmaktDetails(
+    val opphoert: Boolean,
     val fullmaktsgiver: String,
     val fullmektig: String,
     val gyldigFraOgMed: LocalDate,
     val gyldigTilOgMed: LocalDate,
     val fullmaktsgiverNavn: String,
-    val fullmektigsNavn: String
+    val fullmektigsNavn: String,
+    val registrert: ZonedDateTime
 )
 
 data class FullmaktForhold(
@@ -20,11 +23,15 @@ data class FullmaktForhold(
         fun fromFullmaktDetails(details: List<FullmaktDetails>) = FullmaktForhold(
             navn = details.firstOrNull()?.fullmektigsNavn ?: "N/A",
             ident = details.firstOrNull()?.fullmektig ?: "N/A",
-            fullmaktsGivere = details.map {
-                FullmaktsGiver(
-                    ident = it.fullmaktsgiver,
-                    navn = it.fullmaktsgiverNavn
-                )
+            fullmaktsGivere = details
+                .filter { it.opphoert.not() }
+                .sortedByDescending { it.registrert }
+                .distinctBy { it.fullmaktsgiver }
+                .map {
+                    FullmaktsGiver(
+                        ident = it.fullmaktsgiver,
+                        navn = it.fullmaktsgiverNavn
+                    )
             }
         )
     }
