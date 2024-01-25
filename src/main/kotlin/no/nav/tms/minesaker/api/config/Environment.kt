ccb3cfc45b08d81accf4eef6b4f4e3d0d7dd68b7
Christopher Santana <christopher.santana@nav.no>
2024-01-25 14:19:01 +0100

    Smårydding.

--- src/main/kotlin/no/nav/tms/minesaker/api/config/Environment.kt
+++ src/main/kotlin/no/nav/tms/minesaker/api/config/Environment.kt
@@ -1,19 +1,16 @@
 package no.nav.tms.minesaker.api.config
 
-import no.nav.tms.common.util.config.IntEnvVar.getEnvVarAsInt
 import no.nav.tms.common.util.config.StringEnvVar.getEnvVar
 import no.nav.tms.minesaker.api.domain.Sakstemakode
 import java.net.URL
 
 data class Environment(
-    val port: Int = getEnvVarAsInt("PORT", 8080),
     val corsAllowedOrigins: String = getEnvVar("CORS_ALLOWED_ORIGINS"),
     val corsAllowedSchemes: String = getEnvVar("CORS_ALLOWED_SCHEMES", "https"),
     val safEndpoint: URL = URL(getEnvVar("SAF_API_URL")),
     val safClientId: String = getEnvVar("SAF_CLIENT_ID"),
     val digiSosEndpoint: URL = URL(getEnvVar("DIGISOS_API_URL")),
     val digiSosClientId: String = getEnvVar("DIGISOS_CLIENT_ID"),
-    val clusterName: String = getEnvVar("NAIS_CLUSTER_NAME"),
     val sakerUrl: String = getEnvVar("MINE_SAKER_URL"),
     val defaultInnsynLenke: String = getEnvVar("DEFAULT_INNSYN_LENKE"),
     val innsynsLenker: Map<Sakstemakode, String> = mapOf(
